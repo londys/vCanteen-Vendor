c18d8d171aff5750faaf850853007b61a030a28f
Phirawat Chansajcha <davephirawat@gmail.com>
2019-02-21 16:30:33 +0700

    add fonts and try 1 logo

--- app/src/main/java/com/example/vcanteenvendor/MainActivity.java
+++ app/src/main/java/com/example/vcanteenvendor/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.vcanteenvendor;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView t;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test
+
+        b= (Button) findViewById(R.id.button);
+        Typeface myfonts = Typeface.createFromAsset(getAssets(),"fonts/SF-Pro-Text-Semibold.otf");
+        b.setTypeface(myfonts);
     }
 }
