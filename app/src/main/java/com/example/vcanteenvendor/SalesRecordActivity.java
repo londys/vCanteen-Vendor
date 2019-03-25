20534d19b12f78f947ca4b219d1e55a56d7b4e28
Patt Kosit <pattabhum_patt@hotmail.com>
2019-03-25 12:13:30 +0700

    test commit

--- app/src/main/java/com/example/vcanteenvendor/SalesRecordActivity.java
+++ app/src/main/java/com/example/vcanteenvendor/SalesRecordActivity.java
@@ -1,16 +1,30 @@
 package com.example.vcanteenvendor;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
@@ -20,10 +34,12 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SalesRecordActivity extends AppCompatActivity {
 
-
+    private String url = "json url from endpoint";
+    private ListView salesrecord;
     Button orderStatusButton; //ORDER STATUS
     Button menuButton; //MENU
     Button salesRecordButton; //SALES RECORD
@@ -33,7 +49,7 @@ public class SalesRecordActivity extends AppCompatActivity {
     TextView foodName;
     TextView foodExtra;
     TextView foodPrice;
-
+    private ArrayList<SalesRecordArrayList> orders;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,32 +67,13 @@ public class SalesRecordActivity extends AppCompatActivity {
         foodName = (TextView) findViewById(R.id.foodName);
         foodExtra = (TextView) findViewById(R.id.foodExtra);
         foodPrice = (TextView) findViewById(R.id.foodPrice);
-        //////////////////////////////////////////   GET Request   //////////////////////////////////////
-        // https://uxplanet.org/persona-versus-proto-persona-9e26e831ed51
-        /*RequestQueue queue = Volley.newRequestQueue(this);
-        final String url = "http://httpbin.org/get?param1=hello";
 
-        // prepare the Request
-        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>()
-                {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // display response
-                        Log.d("Response", response.toString());
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Error.Response", response);
-                    }
-                }
-        );
+        getSalesRecordArrayList();
 
-        // add it to the RequestQueue
-        queue.add(getRequest);*/
+
+
+
+
 
 
 
@@ -110,12 +107,38 @@ public class SalesRecordActivity extends AppCompatActivity {
         });
 
 
-        String[] name = {"Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Sticky RiceFried Chicken with Sticky Rice","Food3","Food4","Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Food6"};
-        String[] price = {"20.-", "30.-", "40.-", "20.-", "30.-", "50.-" };
+        //String[] name = {"Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Sticky RiceFried Chicken with Sticky Rice","Food3","Food4","Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Food6"};
+        //String[] price = {"20.-", "30.-", "40.-", "20.-", "30.-", "50.-" };
+        /*ArrayList<String> price = new ArrayList<String>();
+        price.add("20.-");
+        price.add("90.-");
+        price.add("2045.-");
+        price.add("42.-");
+        price.add("18.-");
+        price.add("200.-");
+        price.add("30.-");
         String[] extra = {"extra1","extra2", "extra3", "extra4", "extra5","extra6"};
-        ListAdapter testAdapter = new SalesRecordAdapter(this, name, extra, price);
+        ArrayList<String> name = new ArrayList<String>();
+        name.add("A");
+        name.add("B");
+        name.add("C");
+        name.add("D");
+        name.add("E");
+        name.add("F");
+
+
+        ListAdapter testAdapter = new SalesRecordAdapter(this, food);
         ListView salesRecordList = findViewById(R.id.salesRecordList);
-        salesRecordList.setAdapter(testAdapter);
+        salesRecordList.setAdapter(testAdapter);*/
+        // Construct the data source
+        // Create the adapter to convert the array to views
+        //UsersAdapter adapter = new UsersAdapter(this, arrayOfUsers);
+        // Attach the adapter to a ListView
+       // ListView listView = (ListView) findViewById(R.id.lvItems);
+        //listView.setAdapter(adapter);
+
+        //ArrayAdapter<SalesRecordArrayList> itemsAdapter = new ArrayAdapter<String>(this, R.layout.sales_record_row, item);
+
 
     }
 
@@ -141,5 +164,89 @@ public class SalesRecordActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void getSalesRecordArrayList(){
+        final ArrayList<SalesRecordArrayList> orders = new ArrayList<>();
+        final SalesRecordAdapter itemsAdapter = new SalesRecordAdapter(this,orders);
+        final ListView listView = (ListView) findViewById(R.id.salesRecordList);
+        listView.setAdapter(itemsAdapter);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://heroku/vcanteenvendor/v1/sales-record/vendor/sales";
+
+        JSONObject postparams = new JSONObject();
+        try {
+            postparams.put("vendorID", "00001"); //vendorId get from other source later
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequestSalesRecord = new JsonObjectRequest
+                (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        JSONArray jsonArray = null;
+                        //parsing json that isn't an array (total sales & best seller details)
+                        try {
+
+                            JSONObject food_name_best_seller = response.getJSONObject("food_name_best_seller");
+                            String food_name_best_seller_string = food_name_best_seller.getString("food_name_best_seller");
+                            TextView food_name_best_seller_textview = findViewById(R.id.best_seller_dish);
+                            food_name_best_seller_textview.setText(food_name_best_seller_string);
+
+                            JSONObject amount_best_seller = new JSONObject((Map) response);
+                            int amount_best_seller_int = amount_best_seller.getInt("amount_best_seller");
+                            String amount_best_seller_string = Integer.toString(amount_best_seller_int);
+                            TextView amount_best_seller_textview = findViewById(R.id.number_sold);
+                            amount_best_seller_textview.setText(amount_best_seller_string);
+
+                            JSONObject total_daily_sales = new JSONObject((Map) response);
+                            int total_daily_sales_int = total_daily_sales.getInt("total_daily_sales");
+                            String total_daily_sales_string = Integer.toString(total_daily_sales_int);
+                            TextView total_daily_sales_textview = findViewById(R.id.total_sales);
+                            total_daily_sales_textview.setText(total_daily_sales_string);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        try {
+                            JSONArray foodArray = response.getJSONArray("food");
+                            //parsing Json
+                            for(int i=0;i<response.length();i++) {
+                                JSONObject foodObject = foodArray.getJSONObject(i);
+                                int orderId = foodObject.getInt("orderId");
+                                String food_name_appended = foodObject.getString("food_name_appended");
+                                String food_extra_appended = foodObject.getString("food_extra_appended");
+                                int order_price = foodObject.getInt("order_price");
+
+                                SalesRecordArrayList neworder = new SalesRecordArrayList(Integer.toString(orderId), food_name_appended,food_extra_appended,Integer.toString(order_price));
+                                itemsAdapter.add(neworder);
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
+    }
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
--- app/src/main/java/com/example/vcanteenvendor/SalesRecordAdapter.java
+++ app/src/main/java/com/example/vcanteenvendor/SalesRecordAdapter.java
@@ -8,27 +8,146 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class SalesRecordAdapter extends ArrayAdapter {
-    private Activity context;
-    private String[] name;
-    private String[] extra;
-    private String[] price;
+import java.util.ArrayList;
 
-    SalesRecordAdapter(Activity context, String[] name, String[] extra, String[] price ){
+public class SalesRecordAdapter extends ArrayAdapter<SalesRecordArrayList> {
 
-        super(context, R.layout.sales_record_row_relative , name);
+
+    public SalesRecordAdapter (Context context, ArrayList<SalesRecordArrayList> salesRecordList) {
+        super(context, 0, salesRecordList);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        SalesRecordArrayList item = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.sales_record_row, parent, false);
+        }
+        // Lookup view for data population
+        TextView foodid = (TextView) convertView.findViewById(R.id.foodId);
+        TextView foodname = (TextView) convertView.findViewById(R.id.foodName);
+        TextView foodextra = (TextView) convertView.findViewById(R.id.foodExtra);
+        TextView foodprice = (TextView) convertView.findViewById(R.id.foodPrice);
+        // Populate the data into the template view using the data object
+        foodid.setText(item.foodId);
+        foodname.setText(item.foodName);
+        foodextra.setText(item.foodExtra);
+        foodprice.setText(item.foodPrice);
+        // Return the completed view to render on screen
+        return convertView;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*private Activity context;
+    private ArrayList<SalesRecordActivity.salesRecordArrayList> salesRecordArrayList;
+    //private String[] name;
+    //private String[] extra;
+    //private String[] price;
+
+   public SalesRecordAdapter(Activity context, ArrayList<SalesRecordActivity.salesRecordArrayList> salesRecordArrayList ){
+
+        super(context, R.layout.sales_record_row_relative , salesRecordArrayList);
         this.context = context;
-        this.name=name;
-        this.extra=extra;
-        this.price=price;
+        //this.name= name.toArray(new String[0]);
+        this.salesRecordArrayList=salesRecordArrayList;
+        //this.price= price.toArray(new String[0]);
+    }
+    @Override
+    public int getViewTypeCount() {
+        return getCount();
+    }
+    @Override
+    public int getItemViewType(int position) {
+
+        return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent){
-        LayoutInflater orderInflater = LayoutInflater.from(getContext());
-        View customView = orderInflater.inflate(R.layout.sales_record_row_relative, parent, false);
+    public int getCount() {
+        return salesRecordArrayList.size();
+    }
 
-        String singleItem = (String) getItem(position);
+    @Override
+    public SalesRecordActivity.salesRecordArrayList getItem(int position) {
+        return salesRecordArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        // Get the data item for this position
+        SalesRecordActivity.salesRecordArrayList sales = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.sales_record_row_relative, parent, false);
+        }
+        // Lookup view for data population
+        TextView foodname = (TextView) convertView.findViewById(R.id.foodName);
+        TextView foodextra = (TextView) convertView.findViewById(R.id.foodExtra);
+        TextView foodprice = (TextView) convertView.findViewById(R.id.foodPrice);
+        // Populate the data into the template view using the data object
+        foodname.setText(SalesRecordActivity.salesRecordArrayList.);
+        foodprice.setText(user.hometown);
+        foodextra
+        // Return the completed view to render on screen
+        return convertView;*/
+
+
+
+        /*String singleItem = (String) getItem(position);
         TextView foodname = (TextView) customView.findViewById(R.id.foodName);
         TextView foodextra = (TextView) customView.findViewById(R.id.foodExtra);
         TextView foodprice = (TextView) customView.findViewById(R.id.foodPrice);
@@ -37,5 +156,10 @@ public class SalesRecordAdapter extends ArrayAdapter {
         foodextra.setText(extra[position]);
         foodprice.setText(price[position]);
         return customView;
+    }*/
+
+
+    public int getItemCount () {
+        return 0;
     }
 }
