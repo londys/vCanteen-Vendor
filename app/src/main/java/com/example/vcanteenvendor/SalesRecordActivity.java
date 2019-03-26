eb4659b72ba73108f67fa040ca4c2e042751d3bd
Patt <pattabhum_patt@hotmail.com>
2019-03-26 11:40:43 +0700

    Create LoginActivity
    Create xml file for button and picture

--- app/src/main/java/com/example/vcanteenvendor/SalesRecordActivity.java
+++ app/src/main/java/com/example/vcanteenvendor/SalesRecordActivity.java
@@ -3,8 +3,23 @@ package com.example.vcanteenvendor;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SalesRecordActivity extends AppCompatActivity {
 
@@ -14,6 +29,11 @@ public class SalesRecordActivity extends AppCompatActivity {
     Button salesRecordButton; //SALES RECORD
     Button settingsButton; //SETTINGS
 
+    TextView orderNo;
+    TextView foodName;
+    TextView foodExtra;
+    TextView foodPrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +41,43 @@ public class SalesRecordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sales_record);
 
 
+
         orderStatusButton= (Button) findViewById(R.id.orderStatusButton);
         menuButton= (Button) findViewById(R.id.menuButton);
         salesRecordButton= (Button) findViewById(R.id.salesRecordButton);
         settingsButton= (Button) findViewById(R.id.settingsButton);
+
+        orderNo = (TextView) findViewById(R.id.orderNo);
+        foodName = (TextView) findViewById(R.id.foodName);
+        foodExtra = (TextView) findViewById(R.id.foodExtra);
+        foodPrice = (TextView) findViewById(R.id.foodPrice);
+        //////////////////////////////////////////   GET Request   //////////////////////////////////////
+        // https://uxplanet.org/persona-versus-proto-persona-9e26e831ed51
+        /*RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "http://httpbin.org/get?param1=hello";
+
+        // prepare the Request
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.d("Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", response);
+                    }
+                }
+        );
+
+        // add it to the RequestQueue
+        queue.add(getRequest);*/
+
 
 
         //////////////////////////////////////////   Navigation   //////////////////////////////////////
@@ -56,6 +109,13 @@ public class SalesRecordActivity extends AppCompatActivity {
             }
         });
 
+
+        String[] name = {"Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Sticky RiceFried Chicken with Sticky Rice","Food3","Food4","Fried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky RiceFried Chicken with Sticky Rice","Food6"};
+        String[] price = {"20.-", "30.-", "40.-", "20.-", "30.-", "50.-" };
+        String[] extra = {"extra1","extra2", "extra3", "extra4", "extra5","extra6"};
+        ListAdapter testAdapter = new SalesRecordAdapter(this, name, extra, price);
+        ListView salesRecordList = findViewById(R.id.salesRecordList);
+        salesRecordList.setAdapter(testAdapter);
 
     }
 
